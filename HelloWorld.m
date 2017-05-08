f95162abb08b9be2bb04e2e678e5e0fac2d8804a
Anuj Khandelwal <anujonthemove@gmail.com>
2017-05-08 10:27:01 +0530

    Added source code for few languages.

---
+++ HelloWorld.m
@@ -0,0 +1,4 @@
+% Name:     HelloWorld.m
+% Purpose:  Say "Hello World!" in MATLAB
+
+disp('Hello, World!');
---
+++ hello-world.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP Hello World</title>
+</head>
+
+<body>
+<?php
+   echo "<h1> Hello, World! </h1>";
+?>
+</body>
+</html>
